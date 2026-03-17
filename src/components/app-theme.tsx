2a5467626ed7d7d96ce74efeda3e7a95c5463875
NMNM.CC <huangyinhaow@outlook.com>
2026-03-18 02:16:57 +0800

    feat: support dark mode

--- src/components/app-theme.tsx
+++ src/components/app-theme.tsx
@@ -1,8 +1,18 @@
-import { createTheme, ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider, createTheme, useMediaQuery } from "@mui/material";
 import type React from "react";
 
-const theme = createTheme({});
-
 export default function AppTheme({ children }: React.PropsWithChildren) {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const theme = createTheme({
+    palette: {
+      mode: prefersDarkMode ? "dark" : "light",
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
+      {children}
+    </ThemeProvider>
+  );
 }
